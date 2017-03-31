342a91e0347b5e8602004e4a896fd5a1f42d88d9
Higor Alves <Goodyearalves@gmail.com>
2017-03-31 11:32:04 -0300

    Modificações no Gerador
    Foram retirados algumas funções inuteis ou que não precisao estar em
    nosso codigo.

--- Gerador de chaves/gerador.c
+++ Gerador de chaves/gerador.c
@@ -3,36 +3,32 @@
 #include <time.h>
 
 struct base{
-	char chave [2];
 	char frase [100];	
 }typedef Base;
 
 int main(){
 	FILE *arq;
 	FILE *arq1;
-	arq = fopen("frases.txt","r");
-	arq1 = fopen("base_final.txt","w");
-	Base frases[178];
+	arq = fopen("frases","r");
+	arq1 = fopen("base_final","w");
 
+	Base frases[200];
 	int i = 1;
+	int cont;
+	
 	srand(time(NULL));
 
 	if(arq == NULL)
 			printf("Erro, nao foi possivel abrir o arquivo\n");
-	else{		
-		while((fgets(frases[i].frase,100,arq))!= NULL){
-			printf("%s\n",frases[i].frase);
+	else		
+		while((fgets(frases[i].frase,100,arq))!= NULL)
 			i++;
-		}
-	}
 
-	int cont;
 	i=0;	
 
-	for (cont = 0; cont < 200; cont++){
+	for (cont = 0; cont < 201; cont++){
 		i = 1 + rand() % 178;
 		fprintf(arq1, "%c%d %s",65 + rand() % 25, 1 + rand() % 9, frases[i].frase);
 	}
-
 	return 0;
 }
